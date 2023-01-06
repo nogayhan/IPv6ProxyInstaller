14760cdd039ac379f0de6e1b201e816ae57f0319
Yasin Boyraz <yasinboyraz@outlook.com>
2023-01-06 05:36:55 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -145,7 +145,6 @@ sed -i '31r src/define.txt' src/proxy.h
 make -f Makefile.Linux
 cat >~/3proxy/3proxy.cfg <<END
 #!/bin/bash
-
 daemon
 maxconn 10000
 nserver 8.8.8.8
@@ -155,8 +154,10 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 stacksize 6000
+
 auth none
 allow *
+
 flush
 END
 
